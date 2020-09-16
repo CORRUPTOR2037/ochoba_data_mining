21adaee1f5156f39474cd1d0384869aface67ba4
LeLuCh <alekx@alumni.nottingham.ac.uk>
2020-09-16 15:15:11 +0700

    New type of db conn

--- src/common/data_base_wrapper.py
+++ src/common/data_base_wrapper.py
@@ -1,8 +1,11 @@
-from src.common.psycopg_database_wrapper import RemoteDataBaseWrapper 
+from os import environ
+from src.common.psycopg_database_wrapper import RemoteDataBaseWrapper , UrlDataBaseWrapper
 from src.common.sqlite_database_wrapper import SQLiteDataBaseWrapper 
 
 def GetDataBaseWrapper(config):
     if config["type"] == "remote":
         return RemoteDataBaseWrapper(config)
+    elif config["type"] == "url":
+        return UrlDataBaseWrapper(url=environ[config['environ']])
     elif config["type"] == "file":
         return SQLiteDataBaseWrapper(config)
